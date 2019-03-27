263d0661025266173e5baf2fc1bbd649aa250d55
James Litchfield <james.lithfield99@gmail.com>
2019-03-27 19:44:10 +0000

    basic main view layout

--- app/src/main/java/com/group51/uoltimetable/MainActivity.java
+++ app/src/main/java/com/group51/uoltimetable/MainActivity.java
@@ -52,27 +52,9 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -81,6 +63,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_schedule) {
+            // Handle the camera action
         } else if (id == R.id.nav_calender) {
 
         } else if (id == R.id.nav_attendance) {
