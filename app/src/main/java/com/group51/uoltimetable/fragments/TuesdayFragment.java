27d85fcb26f9c0c61896f03df0b4f57b7854659e
James Litchfield <james.lithfield99@gmail.com>
2019-04-07 18:45:52 +0100

    implemented fragments

--- app/src/main/java/com/group51/uoltimetable/fragments/TuesdayFragment.java
+++ app/src/main/java/com/group51/uoltimetable/fragments/TuesdayFragment.java
@@ -1,6 +1,18 @@
 package com.group51.uoltimetable.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.group51.uoltimetable.R;
 
 public class TuesdayFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tuesday_fragment, null);
+    }
 }
