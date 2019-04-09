4f8e2354ca874204d5be2ff26ccc0a0f1ac9df76
Jamie <jamiekennedy97@gmail.com>
2019-04-09 11:32:27 +0100

    Added class to hash strings

---
+++ app/src/main/java/com/group51/uoltimetable/DB/Hash.java
@@ -0,0 +1,29 @@
+import java.security.MessageDigest;
+
+public class Hash{
+    private String input;
+    private static String digits = "0123456789abcdef";
+
+    // When using create hash object with String that needs to be hashed as the input
+    public Hash(String input){
+        this.input = input;
+    }
+
+    // call this method on the hash object to return the hash
+    public String getHash(){
+        try{
+            MessageDigest hash = MessageDigest.getInstance("SHA-256");
+            hash.update(this.input.getBytes());
+            return bytesToHex(hash.digest());
+        } catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuffer result = new StringBuffer();
+        for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        return result.toString();
+    }
+}
