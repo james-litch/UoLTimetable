e347a5a8a866d9c944223c76db6bee97f4abb028
James Litchfield <james.lithfield99@gmail.com>
2019-04-19 11:37:33 +0100

    implemented fragments

--- app/src/main/java/com/group51/uoltimetable/activities/LoginActivity.java
+++ app/src/main/java/com/group51/uoltimetable/activities/LoginActivity.java
@@ -7,37 +7,60 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.group51.uoltimetable.R;
 import com.group51.uoltimetable.utilities.SessionManager;
+import com.group51.uoltimetable.DB.QueryHandler;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     Button loginBtn;
-    EditText emailTxtBox;
+    EditText usernameTxtBox;
     EditText passwordTxtBox;
+    Switch lecturerSwitch;
     SessionManager session;
+    QueryHandler queryHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        loginBtn = (Button) findViewById(R.id.login_button);
-        emailTxtBox = (EditText) findViewById(R.id.email_txt_box);
-        passwordTxtBox = (EditText) findViewById(R.id.password_txt_box);
+        loginBtn = findViewById(R.id.login_button);
+        usernameTxtBox = findViewById(R.id.username_txt_box);
+        passwordTxtBox = findViewById(R.id.password_txt_box);
+        lecturerSwitch = findViewById(R.id.lecturer_switch);
+
         session = new SessionManager(getApplicationContext());
+        //queryHandler = new QueryHandler();
 
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // TODO change this to make it secure and login shit here.
-                String email = emailTxtBox.getText().toString();
+                String username = usernameTxtBox.getText().toString();
                 String password = passwordTxtBox.getText().toString();
-
                 session.createLoginSession();
+                session.isStudent();
                 goToMainActivity();
+
+ /*               if (lecturerSwitch.isChecked() && queryHandler.checkLecturerPassword(username, password)) {
+                    session.createLoginSession();
+                    session.isLecturer();
+                    goToMainActivity();
+                    //TODO change this for a lecturer view.
+                } else if (!lecturerSwitch.isChecked() && queryHandler.checkStudentPassword(username, password)) {
+                    session.createLoginSession();
+                    session.isStudent();
+                    goToMainActivity();
+
+                } else {
+                    Toast.makeText(LoginActivity.this,
+                            "Incorrect credentials", Toast.LENGTH_LONG).show();
+                }*/
 
             }
         });
