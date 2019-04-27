d416c5851558013dda3b800611803dc02080c2ba
MatthewBrayCS <mtthwbr259@gmail.com>
2019-04-27 22:58:27 +0100

    implemented php call to login and login error messages

--- app/src/main/java/com/group51/uoltimetable/activities/LoginActivity.java
+++ app/src/main/java/com/group51/uoltimetable/activities/LoginActivity.java
@@ -8,9 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.group51.uoltimetable.R;
 import com.group51.uoltimetable.utilities.SessionManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.security.AccessController.getContext;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,6 +31,10 @@ public class LoginActivity extends AppCompatActivity {
     EditText passwordTxtBox;
     Switch lecturerSwitch;
     SessionManager session;
+    RequestQueue queue;
+    String url = "https://student.csc.liv.ac.uk/~sgmbray/DBFetch.php";
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
         usernameTxtBox = findViewById(R.id.username_txt_box);
         passwordTxtBox = findViewById(R.id.password_txt_box);
         lecturerSwitch = findViewById(R.id.lecturer_switch);
+        queue = Volley.newRequestQueue(this);
 
         session = new SessionManager(getApplicationContext());
 
@@ -36,15 +53,38 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // TODO change this to make it secure and login shit here.
-                String username = usernameTxtBox.getText().toString();
-                String password = passwordTxtBox.getText().toString();
+                username = usernameTxtBox.getText().toString();
+                password = passwordTxtBox.getText().toString();
 
-                session.createLoginSession(username);
-                session.setAsStudent();
-                //TODO maybe access the name from the database
-                goToMainActivity();
+                StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response == "success") {
+                            session.createLoginSession(username);
+                            session.setAsStudent();
+                            goToMainActivity();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Incorrect Details", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Server Error", Toast.LENGTH_LONG).show();
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("lecturer", Boolean.toString(!lecturerSwitch.isChecked()));
+                        params.put("id", username);
+                        params.put("pword", password);
 
-
+                        return params;
+                    }
+                };;
+                queue.add(request);
             }
         });
     }
